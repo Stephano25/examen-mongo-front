930891d74160c98114e23b6700da956387e8745e
stephano25 <nirina.r@zurcher.edu.mg>
2026-01-08 19:15:45 +0300

    amelioration de designe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { TaskService } from './task/service/task.service';
 import { Task } from './task/model/task.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MatCardModule, MatButtonModule, MatIconModule, MatSelectModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
   tasks: Task[] = [];
+  filter: string = '';
+  sort: string = 'title';
 
   constructor(private service: TaskService) {
     this.load();
   }
 
   load() {
-    this.service.getTasks('', 'title').subscribe((res: Task[]) => {
+    this.service.getTasks(this.filter, this.sort).subscribe((res: Task[]) => {
       this.tasks = res;
     });
   }
 
   finish(id: string) {
     this.service.finish(id).subscribe(() => this.load());
+  }
+
+  setFilter(status: string) {
+    this.filter = status;
+    this.load();
+  }
+
+  sortByName() {
+    this.sort = 'title';
+    this.load();
   }
 }
--- src/app/task/service/task.service.ts
+++ src/app/task/service/task.service.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Task } from '../model/task.model';
 
 @Injectable({ providedIn: 'root' })
